0d9536ebef97eeeb0fa327e29d9dc23c7e9a34b3
T2bex <tobechukwu14@yahoo.com>
2024-01-02 18:38:52 +0100

    Added the review page thingy to the website

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ const toggleBtn = document.getElementById('toggle');
 toggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
+
+//reveal web elment by scrolling
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++){
+        var winheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < winheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
