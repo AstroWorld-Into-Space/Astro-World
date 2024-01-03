f4d57d49f305af9bdd4a1b0752d3bb7e1b1aef9b
Acle <99556572+Acle23@users.noreply.github.com>
2024-01-03 22:15:03 +0100

    Light theme colors

--- script.js
+++ script.js
@@ -16,9 +16,20 @@ function close() {
     mainMenu.style.top = '-100vh';
 }
 
+// dark mode
+
 const body = document.querySelector('.containerWrapper');
 const toggleBtn = document.getElementById('toggle');
+const profileTitleLink1 = document.querySelector('.profileTitleLink1');
+const profileTitleLink2 = document.querySelector('.profileTitleLink2');
+const seperator = document.querySelector('.seperator');
+const profileTitle = document.querySelector('.profileTitle')
 
 toggle.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
+    profileTitleLink1.classList.toggle('light-theme');
+    profileTitleLink2.classList.toggle('light-theme');
+    seperator.classList.toggle('light-theme');
+    openMenu.classList.toggle('light-theme');
+    profileTitle.classList.toggle('light-theme');
 });
